49d238fdcfdd91c34877cff81df58feed6a60e9e
Eduardo Castro <Eduardo Castro>
2015-11-20 10:41:23 -0200

    Build All Screens

--- app/src/main/java/com/eti/nucleus/app10medidas/fragments/TenMeasures.java
+++ app/src/main/java/com/eti/nucleus/app10medidas/fragments/TenMeasures.java
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 import com.eti.nucleus.app10medidas.R;
 import com.eti.nucleus.app10medidas.activity.MainActivity;
 
 public class TenMeasures extends Fragment {
+
+    public Button participate;
+    public Button know_more;
 
     @Nullable
     @Override
@@ -17,9 +23,33 @@ public class TenMeasures extends Fragment {
         return inflater.inflate(R.layout.fragment_ten_measures,container,false);
     }
 
+    /*private void listen_buttons(){
+        participate = (Button) getActivity().findViewById(R.id.ten_measure_participate);
+        participate.setOnClickListener(this);
+        know_more = (Button) getActivity().findViewById(R.id.ten_measure_know_more);
+        know_more.setOnClickListener(this);
+    }*/
+
+    private void set_title(){
+        MainActivity.myToolbar.setTitle(R.string.tenMeasures);
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-        MainActivity.myToolbar.setTitle(R.string.tenMeasures);
+        set_title();
+        //listen_buttons();
     }
+
+    /*@Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ten_measure_know_more:
+                Toast.makeText(getActivity(),"SAIBA MAIS",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ten_measure_participate:
+                Toast.makeText(getActivity(),"PARTICIPE",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }*/
 }
