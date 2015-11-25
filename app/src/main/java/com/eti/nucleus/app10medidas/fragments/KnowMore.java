2749ea3bdd4f7528ed3d58e15e3401b7bf103872
Eduardo Castro <eduhcastro22@gmail.com>
2015-11-25 01:25:47 -0200

    End Ten Measures Fragment Update

--- app/src/main/java/com/eti/nucleus/app10medidas/fragments/KnowMore.java
+++ app/src/main/java/com/eti/nucleus/app10medidas/fragments/KnowMore.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import com.eti.nucleus.app10medidas.R;
@@ -23,32 +22,10 @@ public class KnowMore extends Fragment{
         Log.i("Know More Class", "onAttach()");
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.i("Know More Class", "onResume()");
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_ten_measures_know_more,container,false);
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.i("Know More Class", "onDestroy()");
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Log.i("Know More Class", "onOptionsItemSelected()");
-            default:
-                return true;
-        }
     }
 
 }
