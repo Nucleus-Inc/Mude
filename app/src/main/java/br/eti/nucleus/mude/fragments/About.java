1da1333565386a1fcc91a1fe5774575d98e3ad83
Eduardo Castro <eduhcastro22@gmail.com>
2016-01-18 08:11:05 -0200

    Sobre - Tarefa Completa

--- app/src/main/java/br/eti/nucleus/mude/fragments/About.java
+++ app/src/main/java/br/eti/nucleus/mude/fragments/About.java
@@ -1,5 +1,7 @@
 package br.eti.nucleus.mude.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,11 +10,48 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import br.eti.nucleus.mude.R;
 import br.eti.nucleus.mude.activitys.MainActivity;
 
-public class About extends Fragment {
+public class About extends Fragment implements View.OnClickListener{
+
+    LinearLayout viewPage;
+    LinearLayout likeFanPage;
+    LinearLayout mpf;
+    LinearLayout ifc;
+    LinearLayout materialDrawer;
+
+    private void listen_clickeds() {
+        viewPage = (LinearLayout) getActivity().findViewById(R.id.mude_id_app_layout);
+        if(viewPage!=null){
+            viewPage.setOnClickListener(this);
+            likeFanPage = (LinearLayout) getActivity().findViewById(R.id.mude_id_app_layout_2);
+            if(likeFanPage!=null) {
+                likeFanPage.setOnClickListener(this);
+                mpf = (LinearLayout) getActivity().findViewById(R.id.mude_id_app_layout_3);
+                if (mpf != null) {
+                    mpf.setOnClickListener(this);
+                    ifc = (LinearLayout) getActivity().findViewById(R.id.mude_id_app_layout_4);
+                    if(ifc!=null){
+                        ifc.setOnClickListener(this);
+                        materialDrawer = (LinearLayout) getActivity().findViewById(R.id.mude_id_app_layout_5);
+                        if(materialDrawer!=null)
+                            materialDrawer.setOnClickListener(this);
+                    }
+                }
+            }
+        }
+    }
+
+    private void set_title(){
+        MainActivity.myToolbar.setTitle("Corrupção");
+    }
+
+    private void set_menu_button(){
+        MainActivity.actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+    }
 
     @Nullable
     @Override
@@ -24,7 +63,9 @@ public class About extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        MainActivity.myToolbar.setTitle(R.string.about);
+        set_title();
+        set_menu_button();
+        listen_clickeds();
     }
 
     @Override
@@ -36,5 +77,34 @@ public class About extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_know_more,menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String url = "";
+
+        switch (v.getId()){
+            case R.id.mude_id_app_layout:
+                url = "http://www.combateacorrupcao.mpf.mp.br/10-medidas";
+                break;
+            case R.id.mude_id_app_layout_2:
+                url = "https://www.facebook.com/10medidaseuapoio/?fref=ts";
+                break;
+            case R.id.mude_id_app_layout_3:
+                url = "http://www.mpf.mp.br/";
+                break;
+            case R.id.mude_id_app_layout_4:
+                url = "http://www.ifce.edu.br/";
+                break;
+            case R.id.mude_id_app_layout_5:
+               url = "http://mikepenz.github.io/MaterialDrawer/";
+                break;
+        }
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
+
     }
 }
